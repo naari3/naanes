1e77945bb239a8bce6a5fdfd9aecddeb92dda602
naari3 <naari.named@gmail.com>
2021-03-09 04:20:27 +0900

    Implement get paletter number that HAS BUG

--- src/ppu.rs
+++ src/ppu.rs
@@ -100,7 +100,7 @@ impl PPU {
         // it can be cached
         let tile_number = self.vram[0x2000 + x / 8 + (y / 8) * 0x20];
         let tile = self.get_tile(tile_number);
-        let pal = self.get_palette_number(tile_number);
+        let pal = self.get_palette_number((x / 8 + (y / 8) * 0x20) as u8);
         let c = tile[y % 8][7 - (x % 8)];
 
         if c == 0 {
@@ -139,9 +139,28 @@ impl PPU {
         tile
     }
 
-    fn get_palette_number(&mut self, _tile_number: u8) -> u8 {
-        // todo
-        0
+    fn get_palette_number(&mut self, nametable_number: u8) -> u8 {
+        let attr_addr_lower = nametable_number / 2;
+        let attr_addr_higher = (nametable_number / 0x10) / 2;
+        let attr_addr = attr_addr_lower + attr_addr_higher * 8;
+        if attr_addr != 0 && attr_addr != 0x01 {
+            println!(
+                "attr_addr: 0x{:02X}, nametable_number: 0x{:02X}",
+                attr_addr, nametable_number
+            );
+        }
+        let attr_byte = self.palette_ram.read_byte(attr_addr as usize);
+        let low_addr = nametable_number % 2;
+        let high_addr = (nametable_number / 0x10) % 2;
+        let specific_bits = low_addr + high_addr * 2;
+
+        match specific_bits {
+            0 => attr_byte & 0b00000011,
+            1 => (attr_byte & 0b00001100) >> 2,
+            2 => (attr_byte & 0b00110000) >> 4,
+            3 => (attr_byte & 0b11000000) >> 6,
+            _ => panic!("wrong calc!"),
+        }
     }
 
     pub fn set_rom(&mut self, rom: Vec<u8>, mapper: Mapper) {
