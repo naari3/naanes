052e2be2ea96b927bb8bed20d8ceed8407b5dc46
naari3 <naari.named@gmail.com>
2021-03-25 00:39:35 +0900

    Implement some features

--- standalone/src/main.rs
+++ standalone/src/main.rs
@@ -3,8 +3,8 @@ extern crate naanes;
 use std::time::Instant;
 
 use piston_window::{
-    clear, image as im_pis, Button, CloseEvent, G2dTexture, Key, PistonWindow, PressEvent,
-    ReleaseEvent, RenderEvent, Texture, TextureContext, TextureSettings, Transformed,
+    clear, image as im_pis, Button, CloseEvent, EventLoop, G2dTexture, Key, PistonWindow,
+    PressEvent, ReleaseEvent, RenderEvent, Texture, TextureContext, TextureSettings, Transformed,
     WindowSettings,
 };
 
@@ -15,13 +15,15 @@ fn main() {
 
     let mut display_buffer: [[[u8; 3]; 256]; 240] = [[[0; 3]; 256]; 240];
 
-    let scale = 3.0;
+    let scale = 2.0;
     let mut buffer = image::ImageBuffer::new(256, 240);
+
     let mut window: PistonWindow =
         WindowSettings::new("naanes", [256 as f64 * scale, 240 as f64 * scale])
-            .samples(0)
             .build()
             .unwrap();
+    let mut max_fps_mode = false;
+    window.set_max_fps(68);
     let mut texture_context = TextureContext {
         factory: window.factory.clone(),
         encoder: window.factory.create_command_buffer().into(),
@@ -30,11 +32,12 @@ fn main() {
         Texture::from_image(&mut texture_context, &buffer, &TextureSettings::new()).unwrap();
 
     let mut total_frames = 0;
+    let mut fps = fps_counter::FPSCounter::default();
+
     loop {
         if let Some(event) = window.next() {
             if let Some(_) = event.render_args() {
                 let start = Instant::now();
-
                 nes.step(&mut display_buffer);
 
                 for (x, y, pixel) in buffer.enumerate_pixels_mut() {
@@ -45,17 +48,19 @@ fn main() {
                 // snapshot(display, total_cycles);
 
                 texture.update(&mut texture_context, &buffer).unwrap();
-                window.draw_2d(&event, |context, graphics, device| {
-                    texture_context.encoder.flush(device);
-                    clear([1.0; 4], graphics);
-                    im_pis(&texture, context.transform.scale(scale, scale), graphics);
+                window.draw_2d(&event, |c, g, d| {
+                    texture_context.encoder.flush(d);
+                    clear([0.0; 4], g);
+                    im_pis(&texture, c.transform.scale(scale, scale), g);
                 });
                 total_frames += 1;
-                let frame_duration = start.elapsed();
+
+                let duration = start.elapsed();
                 println!(
-                    "{} frames, fps: {:}",
+                    "{} frames, fps: {:}, took: {:?} ms",
                     total_frames,
-                    1000.0 / frame_duration.as_millis() as f32
+                    fps.tick(),
+                    duration
                 );
             }
 
@@ -69,6 +74,11 @@ fn main() {
                     Key::Down => nes.press_button(naanes::controller::Button::Down),
                     Key::Left => nes.press_button(naanes::controller::Button::Left),
                     Key::Right => nes.press_button(naanes::controller::Button::Right),
+                    Key::Tab => {
+                        max_fps_mode = !max_fps_mode;
+                        let fps = if max_fps_mode { 10000 } else { 68 };
+                        window.set_max_fps(fps);
+                    }
                     _ => {}
                 }
             }
